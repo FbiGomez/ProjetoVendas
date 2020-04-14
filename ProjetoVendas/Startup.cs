0cd9f1c62f33ff4d1964fdd43a88f6c4252bb09b
Fabio Gomez <fabio.eng.mecanica@gmail.com>
2020-04-14 13:59:03 -0300

    CRUD Scaffolding

--- ProjetoVendas/Startup.cs
+++ ProjetoVendas/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ProjetoVendas.Data;
 
 namespace ProjetoVendas
 {
@@ -33,6 +35,9 @@ namespace ProjetoVendas
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoVendasContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoVendasContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
